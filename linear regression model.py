3ef2736971ac363f98e0495db117386275b97052
alanwuuu <67172082+alanwuuu@users.noreply.github.com>
2020-06-28 21:33:44 -0400

    Add files via upload

---
+++ linear regression model.py
@@ -0,0 +1,45 @@
+#linear regression model
+
+def get_gradient_at_b(x, y, b, m):
+  N = len(x)            #our data size
+  diff = 0
+  for i in range(N):
+    x_val = x[i]
+    y_val = y[i]
+    diff += (y_val - ((m * x_val) + b))
+  b_gradient = -(2/N) * diff  
+  return b_gradient
+
+def get_gradient_at_m(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+      x_val = x[i]
+      y_val = y[i]
+      diff += x_val * (y_val - ((m * x_val) + b))
+  m_gradient = -(2/N) * diff  
+  return m_gradient
+
+def step_gradient(b_current, m_current, x, y, learning_rate):
+    b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+    m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+    b = b_current - (learning_rate * b_gradient)
+    m = m_current - (learning_rate * m_gradient)
+    return [b, m]
+  
+def gradient_descent(x, y, learning_rate, num_iterations):
+  b = 0
+  m = 0
+  for i in range(num_iterations):
+    b, m = step_gradient(b, m, x, y, learning_rate)
+  return [b,m]  
+
+
+#example:
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
+learning_rate = 0.01
+num_iterations = 1000
+
+b,m = gradient_descent(months,revenue,learning_rate,num_iterations)
+print('our linear function will be y = {} x + {}'.format(m,b))
